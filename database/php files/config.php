dd95b83df549a53bd8ac913168123bb7d2b08e55
Muneeb Khan <k200375@nu.edu.pk>
2022-11-26 15:54:45 +0500

    aamir mkc

--- database/php files/config.php
+++ database/php files/config.php
@@ -1,7 +1,7 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost:3306');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'data');
