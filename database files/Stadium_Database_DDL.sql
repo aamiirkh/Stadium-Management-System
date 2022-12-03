b9860d05f11833e6ce3692785d6ea8e86ff8a845
Muneeb Khan <k200375@nu.edu.pk>
2022-12-03 18:56:36 +0500

    Database rectified

--- database files/Stadium_Database_DDL.sql
+++ database files/Stadium_Database_DDL.sql
@@ -1,152 +1,235 @@
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE billing (
-    invoice_number INTEGER NOT NULL,
-    rate_per_hour  INT(5) NOT NULL,
-    total_cost     INT(7) NOT NULL
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema stadium_database
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema stadium_database
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `stadium_database` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `stadium_database` ;
+
+-- -----------------------------------------------------
+-- Table `stadium_database`.`team`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stadium_database`.`team` (
+  `team_id` INT NOT NULL,
+  `team_name` VARCHAR(50) NOT NULL,
+  `no_of_team_members` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`team_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
-ALTER TABLE billing ADD CONSTRAINT billing_pk PRIMARY KEY ( invoice_number );
+-- -----------------------------------------------------
+-- Table `stadium_database`.`billing`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stadium_database`.`billing` (
+  `invoice_number` INT NOT NULL,
+  `total_cost` INT NOT NULL,
+  `team_ID` INT NOT NULL,
+  PRIMARY KEY (`invoice_number`),
+  UNIQUE INDEX `team_ID_UNIQUE` (`team_ID` ASC) VISIBLE,
+  CONSTRAINT `billing_team_id_fk`
+    FOREIGN KEY (`team_ID`)
+    REFERENCES `stadium_database`.`team` (`team_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE duration (
-    `Date` 					DATE NOT NULL UNIQUE,
-    start_time              DATE NOT NULL UNIQUE,
-    end_time                DATE NOT NULL,
-    no_of_reservation_hours INTEGER NOT NULL
-);
+
+-- -----------------------------------------------------
+-- Table `stadium_database`.`location`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stadium_database`.`location` (
+  `location_id` INT NOT NULL,
+  `address` VARCHAR(150) NOT NULL,
+  `postal_code` INT NOT NULL,
+  `city` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`location_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE jobs (
-    job_id    varchar(10) NOT NULL,
-    job_title VARCHAR(50) NOT NULL,
-    salary    INTEGER NOT NULL
-);
+-- -----------------------------------------------------
+-- Table `stadium_database`.`stadium`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stadium_database`.`stadium` (
+  `stadium_id` INT NOT NULL,
+  `stadium_name` VARCHAR(100) NOT NULL,
+  `stadium_type` VARCHAR(20) NOT NULL,
+  `stadium_capacity` INT NOT NULL,
+  `location_id` INT NOT NULL,
+  PRIMARY KEY (`stadium_id`),
+  INDEX `stadium_location_fk` (`location_id` ASC) VISIBLE,
+  CONSTRAINT `stadium_location_fk`
+    FOREIGN KEY (`location_id`)
+    REFERENCES `stadium_database`.`location` (`location_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
-ALTER TABLE jobs ADD CONSTRAINT jobs_pk PRIMARY KEY ( job_id );
+-- -----------------------------------------------------
+-- Table `stadium_database`.`reservation`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stadium_database`.`reservation` (
+  `reservation_id` INT NOT NULL,
+  `Date` DATE NOT NULL,
+  `start_time` TIME NOT NULL,
+  `team_id` INT NOT NULL,
+  `stadium_id` INT NOT NULL,
+  PRIMARY KEY (`reservation_id`),
+  INDEX `reservation_stadium_fk` (`stadium_id` ASC) VISIBLE,
+  INDEX `reservation_team_fk` (`team_id` ASC) VISIBLE,
+  CONSTRAINT `reservation_stadium_fk`
+    FOREIGN KEY (`stadium_id`)
+    REFERENCES `stadium_database`.`stadium` (`stadium_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `reservation_team_fk`
+    FOREIGN KEY (`team_id`)
+    REFERENCES `stadium_database`.`team` (`team_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE location (
-    location_id INTEGER NOT NULL,
-    address     VARCHAR(150) NOT NULL,
-    postal_code INTEGER NOT NULL,
-    city        VARCHAR(25) NOT NULL
-);
 
-ALTER TABLE location ADD CONSTRAINT location_pk PRIMARY KEY ( location_id );
+-- -----------------------------------------------------
+-- Table `stadium_database`.`duration`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stadium_database`.`duration` (
+  `reservation_id` INT NOT NULL,
+  `end_time` TIME NOT NULL,
+  `no_of_reservation_hours` INT NOT NULL,
+  PRIMARY KEY (`reservation_id`),
+  CONSTRAINT `duration_reservation_fk`
+    FOREIGN KEY (`reservation_id`)
+    REFERENCES `stadium_database`.`reservation` (`reservation_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE reservation (
-    `Date`         DATE NOT NULL,
-    start_time     DATE NOT NULL,
-    invoice_number INTEGER NOT NULL UNIQUE,
-    team_id        INTEGER NOT NULL,
-    stadium_id     INTEGER NOT NULL UNIQUE
-);
 
-ALTER TABLE reservation ADD CONSTRAINT reservation_pk PRIMARY KEY ( `date`,
-                                                                    start_time );
+-- -----------------------------------------------------
+-- Table `stadium_database`.`jobs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stadium_database`.`jobs` (
+  `job_id` VARCHAR(10) NOT NULL,
+  `job_title` VARCHAR(50) NOT NULL,
+  `salary` INT NOT NULL,
+  PRIMARY KEY (`job_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE seat (
-    seat_type    VARCHAR(15) NOT NULL,
-    ticket_price int(5) NOT NULL
-);
 
-ALTER TABLE seat ADD CONSTRAINT seat_pk PRIMARY KEY ( seat_type );
+-- -----------------------------------------------------
+-- Table `stadium_database`.`seat`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stadium_database`.`seat` (
+  `seat_type` VARCHAR(15) NOT NULL,
+  `ticket_price` INT NOT NULL,
+  PRIMARY KEY (`seat_type`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE stadium (
-    stadium_id       INTEGER NOT NULL,
-    stadium_name     VARCHAR(100) NOT NULL,
-    stadium_type     varchar(20) NOT NULL,
-    stadium_capacity INTEGER NOT NULL,
-    location_id      INTEGER NOT NULL UNIQUE
-);
 
-ALTER TABLE stadium ADD CONSTRAINT stadium_pk PRIMARY KEY ( stadium_id );
+-- -----------------------------------------------------
+-- Table `stadium_database`.`staff`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stadium_database`.`staff` (
+  `staff_id` INT NOT NULL,
+  `f_name` VARCHAR(20) NOT NULL,
+  `l_name` VARCHAR(20) NOT NULL,
+  `phone_number` VARCHAR(11) NOT NULL,
+  `hire_date` DATE NOT NULL,
+  `job_id` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`staff_id`),
+  INDEX `staff_jobs_fk` (`job_id` ASC) VISIBLE,
+  CONSTRAINT `staff_jobs_fk`
+    FOREIGN KEY (`job_id`)
+    REFERENCES `stadium_database`.`jobs` (`job_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE staff (
-    staff_id     INTEGER NOT NULL,
-    f_name       VARCHAR(20) NOT NULL,
-    l_name       VARCHAR(20) NOT NULL,
-    role         VARCHAR(100) NOT NULL,
-    phone_number INTEGER NOT NULL,
-    hire_date    DATE NOT NULL,
-    job_id       VARCHAR(10) NOT NULL
-);
 
-ALTER TABLE staff ADD CONSTRAINT staff_pk PRIMARY KEY ( staff_id );
+-- -----------------------------------------------------
+-- Table `stadium_database`.`team_member`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stadium_database`.`team_member` (
+  `member_id` INT NOT NULL,
+  `team_id` INT NOT NULL,
+  `f_name` VARCHAR(20) NOT NULL,
+  `l_name` VARCHAR(20) NULL DEFAULT NULL,
+  `age` INT NOT NULL,
+  `phone_number` VARCHAR(11) NOT NULL,
+  `sex` CHAR(1) NULL DEFAULT NULL,
+  `role` VARCHAR(25) NULL DEFAULT NULL,
+  PRIMARY KEY (`member_id`),
+  INDEX `team_member_team_fk` (`team_id` ASC) VISIBLE,
+  CONSTRAINT `team_member_team_fk`
+    FOREIGN KEY (`team_id`)
+    REFERENCES `stadium_database`.`team` (`team_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE team (
-    team_id            INTEGER NOT NULL,
-    team_name          VARCHAR(50) NOT NULL,
-    no_of_team_members INTEGER
-);
 
-ALTER TABLE team ADD CONSTRAINT team_pk PRIMARY KEY ( team_id );
+-- -----------------------------------------------------
+-- Table `stadium_database`.`ticket`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stadium_database`.`ticket` (
+  `ticket_id` INT NOT NULL,
+  `reservation_id` INT NOT NULL,
+  `stadium_id` INT NOT NULL,
+  `seat_type` VARCHAR(15) NOT NULL,
+  PRIMARY KEY (`ticket_id`),
+  INDEX `ticket_seat_fk` (`seat_type` ASC) VISIBLE,
+  INDEX `ticket_stadium_fk` (`stadium_id` ASC) VISIBLE,
+  INDEX `ticket_reservation_fk_idx` (`reservation_id` ASC) VISIBLE,
+  CONSTRAINT `ticket_seat_fk`
+    FOREIGN KEY (`seat_type`)
+    REFERENCES `stadium_database`.`seat` (`seat_type`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `ticket_stadium_fk`
+    FOREIGN KEY (`stadium_id`)
+    REFERENCES `stadium_database`.`stadium` (`stadium_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `ticket_reservation_fk`
+    FOREIGN KEY (`reservation_id`)
+    REFERENCES `stadium_database`.`reservation` (`reservation_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE team_member (
-    member_id    INTEGER NOT NULL,
-    team_id      INTEGER NOT NULL,
-    f_name       varchar(20) NOT NULL,
-    l_name       VARCHAR(20),
-    age          INTEGER NOT NULL,
-    phone_number INTEGER NOT NULL,
-    sex          CHAR(1),
-    role         VARCHAR(25)
-);
 
-ALTER TABLE team_member ADD CONSTRAINT team_member_pk PRIMARY KEY ( member_id );
-
-CREATE TABLE ticket (
-    ticket_id  INTEGER NOT NULL,
-    `Date`     DATE NOT NULL,
-    start_time DATE NOT NULL,
-    stadium_id INTEGER NOT NULL,
-    seat_type  VARCHAR(15) NOT NULL
-);
-
-ALTER TABLE ticket ADD CONSTRAINT ticket_pk PRIMARY KEY ( ticket_id );
-
-ALTER TABLE duration
-    ADD CONSTRAINT duration_reservation_fk FOREIGN KEY ( `Date`,
-                                                         start_time )
-        REFERENCES reservation ( `Date`,
-                                 start_time);
-
-ALTER TABLE reservation
-    ADD CONSTRAINT reservation_billing_fk FOREIGN KEY ( invoice_number )
-        REFERENCES billing ( invoice_number )
-            ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE reservation
-    ADD CONSTRAINT reservation_stadium_fk FOREIGN KEY ( stadium_id )
-        REFERENCES stadium ( stadium_id )
-            ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE reservation
-    ADD CONSTRAINT reservation_team_fk FOREIGN KEY ( team_id )
-        REFERENCES team ( team_id )
-           ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE stadium
-    ADD CONSTRAINT stadium_location_fk FOREIGN KEY ( location_id )
-        REFERENCES location ( location_id )
-            ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE staff
-    ADD CONSTRAINT staff_jobs_fk FOREIGN KEY ( job_id )
-        REFERENCES jobs ( job_id )
-            ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE team_member
-    ADD CONSTRAINT team_member_team_fk FOREIGN KEY ( team_id )
-        REFERENCES team ( team_id )
-            ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ticket
-    ADD CONSTRAINT ticket_seat_fk FOREIGN KEY ( seat_type )
-        REFERENCES seat ( seat_type )
-            ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ticket
-    ADD CONSTRAINT ticket_stadium_fk FOREIGN KEY ( stadium_id )
-        REFERENCES stadium ( stadium_id )
-            ON UPDATE CASCADE ON DELETE CASCADE;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ database files/stadium_database.sql
@@ -0,0 +1,379 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 03, 2022 at 01:17 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stadium_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billing`
+--
+
+CREATE TABLE `billing` (
+  `invoice_number` int(11) NOT NULL,
+  `total_cost` int(11) DEFAULT NULL,
+  `team_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `billing`
+--
+
+INSERT INTO `billing` (`invoice_number`, `total_cost`, `team_ID`) VALUES
+(1, NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `duration`
+--
+
+CREATE TABLE `duration` (
+  `reservation_id` int(11) NOT NULL,
+  `end_time` time NOT NULL,
+  `no_of_reservation_hours` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `duration`
+--
+
+INSERT INTO `duration` (`reservation_id`, `end_time`, `no_of_reservation_hours`) VALUES
+(1, '20:00:00', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `job_id` varchar(10) NOT NULL,
+  `job_title` varchar(50) NOT NULL,
+  `salary` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `jobs`
+--
+
+INSERT INTO `jobs` (`job_id`, `job_title`, `salary`) VALUES
+('GDR', 'Gardener', 8000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `location_id` int(11) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `postal_code` int(11) NOT NULL,
+  `city` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`location_id`, `address`, `postal_code`, `city`) VALUES
+(1, 'National Stadium Colony, Gulshan-e-Iqbal', 74600, 'Karachi'),
+(2, 'Shahrah Noor Jahan, Block C North Nazimabad Town', 74700, 'Karachi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `reservation_id` int(11) NOT NULL,
+  `Date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `team_id` int(11) NOT NULL,
+  `stadium_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reservation`
+--
+
+INSERT INTO `reservation` (`reservation_id`, `Date`, `start_time`, `team_id`, `stadium_id`) VALUES
+(1, '2022-11-28', '18:00:00', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seat`
+--
+
+CREATE TABLE `seat` (
+  `seat_type` varchar(15) NOT NULL,
+  `ticket_price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `seat`
+--
+
+INSERT INTO `seat` (`seat_type`, `ticket_price`) VALUES
+('Economy', 1000),
+('First Class', 2000),
+('Lower', 500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stadium`
+--
+
+CREATE TABLE `stadium` (
+  `stadium_id` int(11) NOT NULL,
+  `stadium_name` varchar(100) NOT NULL,
+  `stadium_type` varchar(20) NOT NULL,
+  `stadium_capacity` int(11) NOT NULL,
+  `location_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `stadium`
+--
+
+INSERT INTO `stadium` (`stadium_id`, `stadium_name`, `stadium_type`, `stadium_capacity`, `location_id`) VALUES
+(1, 'National Stadium', 'Cricket', 34228, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `staff_id` int(11) NOT NULL,
+  `f_name` varchar(20) NOT NULL,
+  `l_name` varchar(20) NOT NULL,
+  `phone_number` varchar(11) NOT NULL,
+  `hire_date` date NOT NULL,
+  `job_id` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`staff_id`, `f_name`, `l_name`, `phone_number`, `hire_date`, `job_id`) VALUES
+(1, 'Aamir', 'Bangee', '3331234567', '2022-11-22', 'GDR');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team`
+--
+
+CREATE TABLE `team` (
+  `team_id` int(11) NOT NULL,
+  `team_name` varchar(50) NOT NULL,
+  `no_of_team_members` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `team`
+--
+
+INSERT INTO `team` (`team_id`, `team_name`, `no_of_team_members`) VALUES
+(1, 'Abdul FC', 15);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team_member`
+--
+
+CREATE TABLE `team_member` (
+  `member_id` int(11) NOT NULL,
+  `team_id` int(11) NOT NULL,
+  `f_name` varchar(20) NOT NULL,
+  `l_name` varchar(20) DEFAULT NULL,
+  `age` int(11) NOT NULL,
+  `phone_number` varchar(11) NOT NULL,
+  `sex` char(1) DEFAULT NULL,
+  `role` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `team_member`
+--
+
+INSERT INTO `team_member` (`member_id`, `team_id`, `f_name`, `l_name`, `age`, `phone_number`, `sex`, `role`) VALUES
+(1, 1, 'Abdullah', 'Naeem', 21, '3331236547', 'M', 'Owner');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `ticket_id` int(11) NOT NULL,
+  `reservation_id` int(11) NOT NULL,
+  `seat_type` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ticket`
+--
+
+INSERT INTO `ticket` (`ticket_id`, `reservation_id`, `seat_type`) VALUES
+(1, 1, 'First Class'),
+(2, 1, 'Lower'),
+(3, 1, 'Economy');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `billing`
+--
+ALTER TABLE `billing`
+  ADD PRIMARY KEY (`invoice_number`),
+  ADD UNIQUE KEY `team_ID_UNIQUE` (`team_ID`);
+
+--
+-- Indexes for table `duration`
+--
+ALTER TABLE `duration`
+  ADD PRIMARY KEY (`reservation_id`);
+
+--
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`job_id`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`location_id`);
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`reservation_id`),
+  ADD KEY `reservation_stadium_fk` (`stadium_id`),
+  ADD KEY `reservation_team_fk` (`team_id`);
+
+--
+-- Indexes for table `seat`
+--
+ALTER TABLE `seat`
+  ADD PRIMARY KEY (`seat_type`);
+
+--
+-- Indexes for table `stadium`
+--
+ALTER TABLE `stadium`
+  ADD PRIMARY KEY (`stadium_id`),
+  ADD KEY `stadium_location_fk` (`location_id`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`staff_id`),
+  ADD KEY `staff_jobs_fk` (`job_id`);
+
+--
+-- Indexes for table `team`
+--
+ALTER TABLE `team`
+  ADD PRIMARY KEY (`team_id`);
+
+--
+-- Indexes for table `team_member`
+--
+ALTER TABLE `team_member`
+  ADD PRIMARY KEY (`member_id`),
+  ADD KEY `team_member_team_fk` (`team_id`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`ticket_id`),
+  ADD KEY `ticket_seat_fk` (`seat_type`),
+  ADD KEY `ticket_reservation_fk_idx` (`reservation_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `billing`
+--
+ALTER TABLE `billing`
+  ADD CONSTRAINT `billing_team_id_fk` FOREIGN KEY (`team_ID`) REFERENCES `team` (`team_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `duration`
+--
+ALTER TABLE `duration`
+  ADD CONSTRAINT `duration_reservation_fk` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`reservation_id`);
+
+--
+-- Constraints for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_stadium_fk` FOREIGN KEY (`stadium_id`) REFERENCES `stadium` (`stadium_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservation_team_fk` FOREIGN KEY (`team_id`) REFERENCES `team` (`team_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `stadium`
+--
+ALTER TABLE `stadium`
+  ADD CONSTRAINT `stadium_location_fk` FOREIGN KEY (`location_id`) REFERENCES `location` (`location_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `staff`
+--
+ALTER TABLE `staff`
+  ADD CONSTRAINT `staff_jobs_fk` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`job_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `team_member`
+--
+ALTER TABLE `team_member`
+  ADD CONSTRAINT `team_member_team_fk` FOREIGN KEY (`team_id`) REFERENCES `team` (`team_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `ticket_reservation_fk` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`reservation_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `ticket_seat_fk` FOREIGN KEY (`seat_type`) REFERENCES `seat` (`seat_type`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
