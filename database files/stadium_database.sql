5f3eab63fd62d5c12690bffd08d0edd80ce93fa9
Muneeb Khan <k200375@nu.edu.pk>
2022-12-06 23:08:00 +0500

    Database

--- database files/stadium_database.sql
+++ database files/stadium_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2022 at 01:17 PM
+-- Generation Time: Dec 06, 2022 at 07:06 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -20,8 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `stadium_database`
 --
-CREATE DATABASE stadium_database;
-use stadium_database;
+
 -- --------------------------------------------------------
 
 --
@@ -29,7 +28,7 @@ use stadium_database;
 --
 
 CREATE TABLE `billing` (
-  `invoice_number` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `invoice_number` int(11) NOT NULL,
   `total_cost` int(11) DEFAULT NULL,
   `team_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -38,7 +37,7 @@ CREATE TABLE `billing` (
 -- Dumping data for table `billing`
 --
 
-INSERT INTO `billing` (`invoice_number`, `total_cost`, `team_ID`) VALUES
+INSERT INTO `billing` (`invoice_number`, `total_cost`, `team_id`) VALUES
 (1, NULL, 1);
 
 -- --------------------------------------------------------
@@ -48,7 +47,7 @@ INSERT INTO `billing` (`invoice_number`, `total_cost`, `team_ID`) VALUES
 --
 
 CREATE TABLE `duration` (
-  `reservation_id` int(11) NOT NULL UNIQUE,
+  `reservation_id` int(11) NOT NULL,
   `end_time` time NOT NULL,
   `no_of_reservation_hours` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -67,7 +66,7 @@ INSERT INTO `duration` (`reservation_id`, `end_time`, `no_of_reservation_hours`)
 --
 
 CREATE TABLE `jobs` (
-  `job_id` varchar(10) NOT NULL PRIMARY KEY,
+  `job_id` varchar(10) NOT NULL,
   `job_title` varchar(50) NOT NULL,
   `salary` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -86,7 +85,7 @@ INSERT INTO `jobs` (`job_id`, `job_title`, `salary`) VALUES
 --
 
 CREATE TABLE `location` (
-  `location_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `location_id` int(11) NOT NULL,
   `address` varchar(150) NOT NULL,
   `postal_code` int(11) NOT NULL,
   `city` varchar(25) NOT NULL
@@ -107,7 +106,7 @@ INSERT INTO `location` (`location_id`, `address`, `postal_code`, `city`) VALUES
 --
 
 CREATE TABLE `reservation` (
-  `reservation_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `reservation_id` int(11) NOT NULL,
   `Date` date NOT NULL,
   `start_time` time NOT NULL,
   `team_id` int(11) NOT NULL,
@@ -117,8 +116,6 @@ CREATE TABLE `reservation` (
 --
 -- Dumping data for table `reservation`
 --
-
-ALTER table reservation ADD CONSTRAINT unique_composite_key UNIQUE (Date, start_time, stadium_id);
 
 INSERT INTO `reservation` (`reservation_id`, `Date`, `start_time`, `team_id`, `stadium_id`) VALUES
 (1, '2022-11-28', '18:00:00', 1, 1);
@@ -130,7 +127,7 @@ INSERT INTO `reservation` (`reservation_id`, `Date`, `start_time`, `team_id`, `s
 --
 
 CREATE TABLE `seat` (
-  `seat_type` varchar(15) NOT NULL PRIMARY KEY,
+  `seat_type` varchar(15) NOT NULL,
   `ticket_price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -150,7 +147,7 @@ INSERT INTO `seat` (`seat_type`, `ticket_price`) VALUES
 --
 
 CREATE TABLE `stadium` (
-  `stadium_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `stadium_id` int(11) NOT NULL,
   `stadium_name` varchar(100) NOT NULL,
   `stadium_type` varchar(20) NOT NULL,
   `stadium_capacity` int(11) NOT NULL,
@@ -171,7 +168,7 @@ INSERT INTO `stadium` (`stadium_id`, `stadium_name`, `stadium_type`, `stadium_ca
 --
 
 CREATE TABLE `staff` (
-  `staff_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `staff_id` int(11) NOT NULL,
   `f_name` varchar(20) NOT NULL,
   `l_name` varchar(20) NOT NULL,
   `phone_number` varchar(11) NOT NULL,
@@ -193,7 +190,7 @@ INSERT INTO `staff` (`staff_id`, `f_name`, `l_name`, `phone_number`, `hire_date`
 --
 
 CREATE TABLE `team` (
-  `team_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `team_id` int(11) NOT NULL,
   `team_name` varchar(50) NOT NULL,
   `no_of_team_members` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -212,7 +209,7 @@ INSERT INTO `team` (`team_id`, `team_name`, `no_of_team_members`) VALUES
 --
 
 CREATE TABLE `team_member` (
-  `member_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `member_id` int(11) NOT NULL,
   `team_id` int(11) NOT NULL,
   `f_name` varchar(20) NOT NULL,
   `l_name` varchar(20) DEFAULT NULL,
@@ -236,7 +233,7 @@ INSERT INTO `team_member` (`member_id`, `team_id`, `f_name`, `l_name`, `age`, `p
 --
 
 CREATE TABLE `ticket` (
-  `ticket_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `ticket_id` int(11) NOT NULL,
   `reservation_id` int(11) NOT NULL,
   `seat_type` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -258,59 +255,128 @@ INSERT INTO `ticket` (`ticket_id`, `reservation_id`, `seat_type`) VALUES
 -- Indexes for table `billing`
 --
 ALTER TABLE `billing`
-  ADD UNIQUE KEY `team_ID_UNIQUE` (`team_ID`);
+  ADD PRIMARY KEY (`invoice_number`),
+  ADD UNIQUE KEY `team_ID_UNIQUE` (`team_id`);
 
 --
 -- Indexes for table `duration`
 --
+ALTER TABLE `duration`
+  ADD UNIQUE KEY `reservation_id` (`reservation_id`);
 
 --
 -- Indexes for table `jobs`
 --
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`job_id`);
 
 --
 -- Indexes for table `location`
 --
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`location_id`);
 
 --
 -- Indexes for table `reservation`
 --
 ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`reservation_id`),
+  ADD UNIQUE KEY `unique_composite_key` (`Date`,`start_time`,`stadium_id`),
   ADD KEY `reservation_stadium_fk` (`stadium_id`),
   ADD KEY `reservation_team_fk` (`team_id`);
 
 --
 -- Indexes for table `seat`
 --
+ALTER TABLE `seat`
+  ADD PRIMARY KEY (`seat_type`);
 
 --
 -- Indexes for table `stadium`
 --
 ALTER TABLE `stadium`
+  ADD PRIMARY KEY (`stadium_id`),
   ADD KEY `stadium_location_fk` (`location_id`);
 
 --
 -- Indexes for table `staff`
 --
 ALTER TABLE `staff`
+  ADD PRIMARY KEY (`staff_id`),
   ADD KEY `staff_jobs_fk` (`job_id`);
 
 --
 -- Indexes for table `team`
 --
+ALTER TABLE `team`
+  ADD PRIMARY KEY (`team_id`);
 
 --
 -- Indexes for table `team_member`
 --
 ALTER TABLE `team_member`
+  ADD PRIMARY KEY (`member_id`),
   ADD KEY `team_member_team_fk` (`team_id`);
 
 --
 -- Indexes for table `ticket`
 --
 ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`ticket_id`),
   ADD KEY `ticket_seat_fk` (`seat_type`),
   ADD KEY `ticket_reservation_fk_idx` (`reservation_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `billing`
+--
+ALTER TABLE `billing`
+  MODIFY `invoice_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `stadium`
+--
+ALTER TABLE `stadium`
+  MODIFY `stadium_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `team`
+--
+ALTER TABLE `team`
+  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `team_member`
+--
+ALTER TABLE `team_member`
+  MODIFY `member_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `ticket_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -320,9 +386,14 @@ ALTER TABLE `ticket`
 -- Constraints for table `billing`
 --
 ALTER TABLE `billing`
-  ADD CONSTRAINT `billing_team_id_fk` FOREIGN KEY (`team_ID`) REFERENCES `team` (`team_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `billing_team_id_fk` FOREIGN KEY (`team_id`) REFERENCES `team` (`team_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `duration`
+--
+ALTER TABLE `duration`
+  ADD CONSTRAINT `duration_reservation_fk` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`reservation_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 --
 -- Constraints for table `reservation`
 --
@@ -354,10 +425,6 @@ ALTER TABLE `team_member`
 ALTER TABLE `ticket`
   ADD CONSTRAINT `ticket_reservation_fk` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`reservation_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `ticket_seat_fk` FOREIGN KEY (`seat_type`) REFERENCES `seat` (`seat_type`) ON DELETE CASCADE ON UPDATE CASCADE;
--- Constraints for table `duration`
---
-ALTER TABLE duration
-  ADD CONSTRAINT duration_reservation_fk FOREIGN KEY (reservation_id) REFERENCES reservation (reservation_id) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
